3644dcf7ec74c882d17505e6d8940789080001ff
Sergey Vinogradov <grapesh@gmail.com>
2017-09-26 13:37:36 -0400

    Update plotter.py

--- csdlpy/plotter/plotter.py
+++ csdlpy/plotter/plotter.py
@@ -299,7 +299,7 @@ def plot_estofs_timeseries (obs_dates,      obs_values,
             peak_ft  = 3.28*peak_val
             peak_dat = mod_dates[np.argmax(mod_values)]
             plt.plot(peak_dat, peak_val, 'bo')
-            plt.text(peak_dat, 1.05*peak_val, str(np.round(peak_val,1)) + "m, (" + str(np.round(peak_ft,1)) +"ft) MSL", color='b')
+            plt.text(peak_dat, 1.05*peak_val, str(np.round(peak_val,1)) + "m (" + str(np.round(peak_ft,1)) +"ft) MSL", color='b')
             plt.plot([obs_dates[0], mod_dates[-1]], [peak_val, peak_val], '--b')
             plt.plot([peak_dat, peak_dat], [ylim[0], ylim[1]], '--b')
             
