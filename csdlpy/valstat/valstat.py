b7d7e8beafef4025d51e824315a8a9df1b2abd40
Sergey Vinogradov <grapesh@gmail.com>
2017-10-07 14:06:14 -0400

    Included metrics computations

--- csdlpy/valstat/valstat.py
+++ csdlpy/valstat/valstat.py
@@ -31,22 +31,63 @@ def rms(V):
     return np.sqrt( summ/N )
 
 #==============================================================================
+def var_explained(m, d):
+    """
+    Returns amount of variance in d that is explained by variance in m
+    """    
+    stdd = np.nanstd(d)
+    eps  = 100.*(stdd - np.nanstd(d-m))/stdd
+    if eps < 0:
+        eps = 0.
+    if eps > 100.:
+        eps = 100
+    return eps
+
+#==============================================================================
+def skill(m, d):
+    """
+    Returns the skill
+    """    
+    skill = 1-np.nansum((m-d)**2)/ np.nansum( (np.abs(m-np.nanmean(d)) + np.abs(d-np.nanmean(d)))**2 )
+    return skill
+
+#==============================================================================
+def rvalue(m, d):
+    """
+    Returns R-value (Pearson correlation coefficient)
+    """    
+    rval = np.nansum( (d-np.nanmean(d))*(m-np.nanmean(d)) ) / \
+                    (np.sqrt(np.nansum( (d-np.nanmean(d))**2 ))* \
+                     np.sqrt(np.nansum( (m-np.nanmean(m))**2 )) )
+    return rval
+
+#==============================================================================
 def metrics (data, model, dates):
     """    
     data and model (np.arrays) projected on the 
     same time scale 'dates' (datetime)
     Computes: 
-        rmsd, peak, plag (in minutes)    
+        rmsd - root mean square difference, in data units 
+        peak - difference in max values in data and model, in data units
+        plag - difference in occurrence time of the maxima, in minutes
+        bias - linear bias, in data units 
+        vexp - variance explained, in %
+        skil - skill, unitless
+        rval - R-value, unitless
     """
-    rmsd = rms(data-model)
-    peak = np.nanmax(data) - np.nanmax(model)
-    plag = 60.*(dates[np.argmax(data)] - dates[np.argmax(model)]).total_seconds 
-    
+    rmsd = rms(model-data)
+    peak = np.nanmax(model) - np.nanmax(data)
+    plag = (dates[np.argmax(model)] - dates[np.argmax(data)]).total_seconds() 
+    plag = plag/60. #in minutes
+    bias = np.nanmean(model) - np.nanmean(data)
+    vexp = var_explained (model, data)
+    skil = skill (model, data)
+    rval = rvalue(model, data)
+    #npts 
     return {'rmsd': rmsd, 
             'peak': peak,
-            'plag': plag}
-#            'vexp': vexp,
-#            'bias': bias,
-#            'rval': rval,
-#            'skil': skil,
-#            'npts': npts}
+            'plag': plag,
+            'bias': bias,
+            'vexp': vexp,
+            'skil': skil,
+            'rval': rval}
